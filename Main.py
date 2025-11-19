f5c3885548147eee239ceed51dc10cc34949da69
knedderm1 <knedderm@iastate.edu>
2025-11-19 10:36:00 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,28 +1,27 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="O18 Modeling",
+    page_icon="💦",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("Modeling O18 Fractionation Over Time")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+   In nature, Oxygen Isotopes can commonly be found as O16 and O18 respectively.
+   There are many fluxes that contribute to the variation in the isotopic make up of
+   the oceans and atmosphere. O18 concentrations within the ocean may have 
+   varied throughout time as a result of said fluxes.
+   
+   Variation of Oxygen isotopes can be indicative of various changes. 
+   O18 isotopes can be used to predict paleo-altitudes as heavier water molecules fall preferentially first, 
+   leaving lighter molecules at higher altitudes. We can also use this to predict continental emergence.
+   
+   The following models attempt to use our knowledge of various oxygen related fluxes to model the O18 proportion of oceans over time.
+   For accuracy, we attempt to compare this to other methods of determining O18 concentrations such as inversion estimates of hydrothermal waters.
+   Take a look at the various models on the sidebar. Each attempts to determine O18 using a separate model, with user input to vary parameters and observe the results.   
 """
 )
